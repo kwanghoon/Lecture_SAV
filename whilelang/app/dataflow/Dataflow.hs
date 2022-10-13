1a008f3ae7e3622469fbe823dadbb929f48837f2
Kwanghoon Choi <lazyswamp@gmail.com>
2022-10-13 23:13:41 +0900

    adjust label numbers

--- whilelang/app/dataflow/Dataflow.hs
+++ whilelang/app/dataflow/Dataflow.hs
@@ -38,27 +38,27 @@ mkSeq [] = error $ "mkSeq: Unexpected empty list"
 --
 numbering :: Int -> Comm -> Map.Map Int LabeledComm -> (Int, Map.Map Int LabeledComm)
 
-numbering entry CSkip map = (entry+1, Map.insert entry LCSkip map)
+numbering entry CSkip map = (entry, Map.insert entry LCSkip map)
 
 numbering entry (CSeq comm1 comm2) map =
   let (exit1, map1) = numbering entry comm1 map
-      (exit2, map2) = numbering exit1 comm2 map1
-  in  (exit2 + 2, Map.insert (exit2 + 1) (LCSeq (exit1 - 1) (exit2 - 1)) map2)
+      (exit2, map2) = numbering (exit1+1) comm2 map1
+  in  (exit2 + 1, Map.insert (exit2 + 1) (LCSeq exit1 exit2) map2)
 
 numbering entry (CAssign varName expr) map =
-  (entry+1, Map.insert entry (LCAssign varName expr) map)
+  (entry, Map.insert entry (LCAssign varName expr) map)
 
 numbering entry (CRead varName) map =
-  (entry+1, Map.insert entry (LCRead varName) map)
+  (entry, Map.insert entry (LCRead varName) map)
 
 numbering entry (CWrite expr) map =
-  (entry+1, Map.insert entry (LCWrite expr) map)
+  (entry, Map.insert entry (LCWrite expr) map)
 
 numbering entry (CIf expr comm1 comm2) map =
   let (exit1, map1) = numbering entry comm1 map
-      (exit2, map2) = numbering exit1 comm2 map1
-  in  (exit2+1, Map.insert exit2 (LCIf expr (exit1 - 1) (exit2 - 1)) map2)
+      (exit2, map2) = numbering (exit1+1) comm2 map1
+  in  (exit2+1, Map.insert (exit2+1) (LCIf expr exit1 exit2) map2)
 
 numbering entry (CWhile expr comm) map =
   let (exit, map1) = numbering entry comm map
-  in  (exit + 1, Map.insert exit (LCWhile expr (exit - 1)) map1)
+  in  (exit + 1, Map.insert (exit + 1) (LCWhile expr exit) map1)
