d703d0d0cc868bb0b8aaf740ef0d0f2e9c2e72d5
Kwanghoon Choi <lazyswamp@gmail.com>
2022-11-26 18:02:22 +0900

    test cases

--- whilelang/test/Spec.hs
+++ whilelang/test/Spec.hs
@@ -4,15 +4,15 @@ import Util
 
 files =
   [ "assign.while"
-  , "bad.while"
-  , "bad1.while"
-  , "bad2.while"
-  , "bad3.while"
-  , "bad4.while"
-  , "bad5.while"
+--  , "bad.while"
+--  , "bad1.while"
+--  , "bad2.while"
+--  , "bad3.while"
+--  , "bad4.while"
+--  , "bad5.while"
   , "factorial.while"
   , "ifcond1.while"
-  , "ifcond1_nottypechecked.while"
+--  , "ifcond1_nottypechecked.while"
   , "ifcond2.while"
   , "sequence.while"
   , "while1.while"
@@ -25,7 +25,12 @@ base = "./example/"
 paths = [ base ++ file | file <- files]
 
 main :: IO ()
-main = testParsing paths
+main =
+ do mapM_ doParsing paths
+    mapM_ doTypecheck paths
+    mapM_ doAnalysis paths
+    mapM_ doSymbolic paths
+    
 
 testParsing :: [String] -> IO ()
 testParsing fileNames = mapM_ doParsing fileNames
